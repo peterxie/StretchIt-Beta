737874d7b96fa79ab52611642117facfd0fcab12
Michael Meyerson <mikemey@umich.edu>
2014-11-13 00:00:20 -0500

    Gesture, Help, MainMenu, MenuPage, Statistics

---
+++ DepthBasics-WPF/Help.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StretchIt
+{
+    public class Help_t : MenuPage_t
+    {
+        public void back()
+        {
+
+        }
+
+        //not listed in design doc, but assuming we need this
+        public void display()
+        {
+
+        }
+    }
+}
---
+++ DepthBasics-WPF/Statistics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace StretchIt
+{
+    public class Statistics_t : MenuPage_t
+    {
+        private string path;
+        private int all_longest_streak; //all-time
+        private double all_percent_correct;
+        private int rec_longest_streak; //recent
+        private double rec_percent_correct;
+
+        public void back()
+        {
+
+        }
+
+        //not listed in design doc, but assuming we need this
+        public void display()
+        {
+
+        }
+
+        public void loadStatistics()
+        {
+            StreamReader file = new StreamReader(path);
+            all_longest_streak = int.Parse(file.ReadLine());
+            all_percent_correct = double.Parse(file.ReadLine());
+            rec_longest_streak = int.Parse(file.ReadLine());
+            rec_percent_correct = double.Parse(file.ReadLine());
+        }
+
+        public void saveStatistics()
+        {
+            StreamWriter file = new StreamWriter(path);
+            file.WriteLine(all_longest_streak);
+            file.WriteLine(all_percent_correct);
+            file.WriteLine(rec_longest_streak);
+            file.WriteLine(rec_percent_correct);
+        }
+    }
+}
