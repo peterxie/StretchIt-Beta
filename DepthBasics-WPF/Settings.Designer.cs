77de26896ade5f28799fef76f6eab348063679fb
Michael Meyerson <mikemey@umich.edu>
2014-11-16 17:40:36 -0500

    made it look real nice

--- DepthBasics-WPF/Settings.Designer.cs
+++ DepthBasics-WPF/Settings.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
             this.title = new System.Windows.Forms.Label();
             this.Back = new System.Windows.Forms.Button();
             this.pushLabel = new System.Windows.Forms.Label();
@@ -56,7 +57,9 @@
             // title
             // 
             this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
             this.title.Font = new System.Drawing.Font("Action Jackson", 47.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.title.Location = new System.Drawing.Point(135, 38);
             this.title.Name = "title";
             this.title.Size = new System.Drawing.Size(348, 65);
@@ -65,7 +68,7 @@
             // 
             // Back
             // 
-            this.Back.Location = new System.Drawing.Point(12, 327);
+            this.Back.Location = new System.Drawing.Point(12, 364);
             this.Back.Name = "Back";
             this.Back.Size = new System.Drawing.Size(75, 23);
             this.Back.TabIndex = 2;
@@ -76,7 +79,9 @@
             // pushLabel
             // 
             this.pushLabel.AutoSize = true;
-            this.pushLabel.Location = new System.Drawing.Point(178, 139);
+            this.pushLabel.BackColor = System.Drawing.Color.Transparent;
+            this.pushLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pushLabel.Location = new System.Drawing.Point(223, 137);
             this.pushLabel.Name = "pushLabel";
             this.pushLabel.Size = new System.Drawing.Size(31, 13);
             this.pushLabel.TabIndex = 4;
@@ -85,7 +90,9 @@
             // pullLabel
             // 
             this.pullLabel.AutoSize = true;
-            this.pullLabel.Location = new System.Drawing.Point(178, 165);
+            this.pullLabel.BackColor = System.Drawing.Color.Transparent;
+            this.pullLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pullLabel.Location = new System.Drawing.Point(223, 163);
             this.pullLabel.Name = "pullLabel";
             this.pullLabel.Size = new System.Drawing.Size(24, 13);
             this.pullLabel.TabIndex = 6;
@@ -93,16 +100,18 @@
             // 
             // pullUpDown
             // 
-            this.pullUpDown.Location = new System.Drawing.Point(316, 159);
+            this.pullUpDown.Location = new System.Drawing.Point(331, 156);
             this.pullUpDown.Name = "pullUpDown";
-            this.pullUpDown.Size = new System.Drawing.Size(120, 20);
+            this.pullUpDown.Size = new System.Drawing.Size(43, 20);
             this.pullUpDown.TabIndex = 5;
             this.pullUpDown.ValueChanged += new System.EventHandler(this.pullUpDown_ValueChanged);
             // 
             // swipeLabel
             // 
             this.swipeLabel.AutoSize = true;
-            this.swipeLabel.Location = new System.Drawing.Point(178, 191);
+            this.swipeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.swipeLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.swipeLabel.Location = new System.Drawing.Point(223, 189);
             this.swipeLabel.Name = "swipeLabel";
             this.swipeLabel.Size = new System.Drawing.Size(36, 13);
             this.swipeLabel.TabIndex = 8;
@@ -110,16 +119,18 @@
             // 
             // swipeUpDown
             // 
-            this.swipeUpDown.Location = new System.Drawing.Point(316, 185);
+            this.swipeUpDown.Location = new System.Drawing.Point(331, 182);
             this.swipeUpDown.Name = "swipeUpDown";
-            this.swipeUpDown.Size = new System.Drawing.Size(120, 20);
+            this.swipeUpDown.Size = new System.Drawing.Size(43, 20);
             this.swipeUpDown.TabIndex = 7;
             this.swipeUpDown.ValueChanged += new System.EventHandler(this.swipeUpDown_ValueChanged);
             // 
             // pushHardLabel
             // 
             this.pushHardLabel.AutoSize = true;
-            this.pushHardLabel.Location = new System.Drawing.Point(178, 269);
+            this.pushHardLabel.BackColor = System.Drawing.Color.Transparent;
+            this.pushHardLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pushHardLabel.Location = new System.Drawing.Point(223, 267);
             this.pushHardLabel.Name = "pushHardLabel";
             this.pushHardLabel.Size = new System.Drawing.Size(57, 13);
             this.pushHardLabel.TabIndex = 14;
@@ -127,16 +138,18 @@
             // 
             // pushHardUpDown
             // 
-            this.pushHardUpDown.Location = new System.Drawing.Point(316, 263);
+            this.pushHardUpDown.Location = new System.Drawing.Point(331, 260);
             this.pushHardUpDown.Name = "pushHardUpDown";
-            this.pushHardUpDown.Size = new System.Drawing.Size(120, 20);
+            this.pushHardUpDown.Size = new System.Drawing.Size(43, 20);
             this.pushHardUpDown.TabIndex = 13;
             this.pushHardUpDown.ValueChanged += new System.EventHandler(this.pushHardUpDown_ValueChanged);
             // 
             // fistBumpLabel
             // 
             this.fistBumpLabel.AutoSize = true;
-            this.fistBumpLabel.Location = new System.Drawing.Point(178, 243);
+            this.fistBumpLabel.BackColor = System.Drawing.Color.Transparent;
+            this.fistBumpLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.fistBumpLabel.Location = new System.Drawing.Point(223, 241);
             this.fistBumpLabel.Name = "fistBumpLabel";
             this.fistBumpLabel.Size = new System.Drawing.Size(53, 13);
             this.fistBumpLabel.TabIndex = 12;
@@ -144,16 +157,18 @@
             // 
             // fistBumpUpDown
             // 
-            this.fistBumpUpDown.Location = new System.Drawing.Point(316, 237);
+            this.fistBumpUpDown.Location = new System.Drawing.Point(331, 234);
             this.fistBumpUpDown.Name = "fistBumpUpDown";
-            this.fistBumpUpDown.Size = new System.Drawing.Size(120, 20);
+            this.fistBumpUpDown.Size = new System.Drawing.Size(43, 20);
             this.fistBumpUpDown.TabIndex = 11;
             this.fistBumpUpDown.ValueChanged += new System.EventHandler(this.firstBumpUpDown_ValueChanged);
             // 
             // highFiveLabel
             // 
             this.highFiveLabel.AutoSize = true;
-            this.highFiveLabel.Location = new System.Drawing.Point(178, 217);
+            this.highFiveLabel.BackColor = System.Drawing.Color.Transparent;
+            this.highFiveLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.highFiveLabel.Location = new System.Drawing.Point(223, 215);
             this.highFiveLabel.Name = "highFiveLabel";
             this.highFiveLabel.Size = new System.Drawing.Size(52, 13);
             this.highFiveLabel.TabIndex = 10;
@@ -161,16 +176,18 @@
             // 
             // highFiveUpDown
             // 
-            this.highFiveUpDown.Location = new System.Drawing.Point(316, 211);
+            this.highFiveUpDown.Location = new System.Drawing.Point(331, 208);
             this.highFiveUpDown.Name = "highFiveUpDown";
-            this.highFiveUpDown.Size = new System.Drawing.Size(120, 20);
+            this.highFiveUpDown.Size = new System.Drawing.Size(43, 20);
             this.highFiveUpDown.TabIndex = 9;
             this.highFiveUpDown.ValueChanged += new System.EventHandler(this.highFiveUpDown_ValueChanged);
             // 
             // pullHardLabel
             // 
             this.pullHardLabel.AutoSize = true;
-            this.pullHardLabel.Location = new System.Drawing.Point(178, 296);
+            this.pullHardLabel.BackColor = System.Drawing.Color.Transparent;
+            this.pullHardLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.pullHardLabel.Location = new System.Drawing.Point(223, 294);
             this.pullHardLabel.Name = "pullHardLabel";
             this.pullHardLabel.Size = new System.Drawing.Size(50, 13);
             this.pullHardLabel.TabIndex = 16;
@@ -178,17 +195,17 @@
             // 
             // pullHardUpDown
             // 
-            this.pullHardUpDown.Location = new System.Drawing.Point(316, 290);
+            this.pullHardUpDown.Location = new System.Drawing.Point(331, 287);
             this.pullHardUpDown.Name = "pullHardUpDown";
-            this.pullHardUpDown.Size = new System.Drawing.Size(120, 20);
+            this.pullHardUpDown.Size = new System.Drawing.Size(43, 20);
             this.pullHardUpDown.TabIndex = 15;
             this.pullHardUpDown.ValueChanged += new System.EventHandler(this.pullHardUpDown_ValueChanged);
             // 
             // pushUpDown
             // 
-            this.pushUpDown.Location = new System.Drawing.Point(316, 132);
+            this.pushUpDown.Location = new System.Drawing.Point(331, 130);
             this.pushUpDown.Name = "pushUpDown";
-            this.pushUpDown.Size = new System.Drawing.Size(120, 20);
+            this.pushUpDown.Size = new System.Drawing.Size(43, 20);
             this.pushUpDown.TabIndex = 17;
             this.pushUpDown.ValueChanged += new System.EventHandler(this.pushUpDown_ValueChanged);
             // 
@@ -196,6 +213,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(599, 399);
             this.Controls.Add(this.pushUpDown);
             this.Controls.Add(this.pullHardLabel);
---
+++ DepthBasics-WPF/StatisticsForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Microsoft.Samples.Kinect.DepthBasics
+{
+    public partial class StatisticsForm : Form
+    {
+        public StatisticsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
