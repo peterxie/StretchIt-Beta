aab49a3cf644a5f88aaabc579b538068794f3c40
Nicholas Tountasakis <ntountas@umich.edu>
2014-11-16 14:04:56 -0500

    Neube wanted this

--- DepthBasics-WPF/GlobalVar.cs
+++ DepthBasics-WPF/GlobalVar.cs
@@ -9,12 +9,15 @@ namespace StretchIt
         public static string REFERENCE_GESTURE_DIRECTORY_C;
         public static string AV_DIRECTORY_C;
         public static List<string> ALL_POSSIBLE_GESTURES_C;
-        public static int NUM_PIXELS_C; 
+        public static int NUM_PIXELS_C;
+        public static Frame_t GLOBAL_KINECT_FRAME;
         public GlobalVar()
         {
             REFERENCE_GESTURE_DIRECTORY_C = @"c:.\Reference_Gestures\";
             AV_DIRECTORY_C = @"c:.\AudioVideo\";
             NUM_PIXELS_C = 307200;
+            ALL_POSSIBLE_GESTURES_C = new List<string>();
+            GLOBAL_KINECT_FRAME = new Frame_t(NUM_PIXELS_C);
 
             string[] filePaths = Directory.GetFiles(REFERENCE_GESTURE_DIRECTORY_C);
             for (int i = 0; i < filePaths.Length; ++i)
