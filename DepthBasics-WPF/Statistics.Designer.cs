8dbe6079df2d22468460a2baf52ceb6334cb6669
Matthew Neubauer <mneub@umich.edu>
2014-11-17 12:49:30 -0500

    Renamed statistics files and class name

--- DepthBasics-WPF/Statistics.Designer.cs
+++ DepthBasics-WPF/Statistics.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace StretchIt
 {
-    partial class StatisticsForm
+    partial class Statistics_t
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StatisticsForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Statistics_t));
             this.StatisticsLabel = new System.Windows.Forms.Label();
             this.streakAllTimeLabel = new System.Windows.Forms.Label();
             this.streakMonthlyLabel = new System.Windows.Forms.Label();
