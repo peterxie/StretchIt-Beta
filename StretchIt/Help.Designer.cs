d83517c31436dfb5122ff46252b5cc58d0229100
Matthew Neubauer <mneub@umich.edu>
2014-11-18 18:39:04 -0500

    Help menu functionality added
    The design is not great, should probably be edited.

--- StretchIt/Help.Designer.cs
+++ StretchIt/Help.Designer.cs
@@ -32,19 +32,23 @@
             this.Title = new System.Windows.Forms.Label();
             this.backLabel = new System.Windows.Forms.Label();
             this.helpText = new System.Windows.Forms.Label();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.statisticsLabel = new System.Windows.Forms.Label();
+            this.settingsLabel = new System.Windows.Forms.Label();
+            this.howToLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Title
             // 
             this.Title.AutoSize = true;
             this.Title.BackColor = System.Drawing.Color.Transparent;
-            this.Title.Font = new System.Drawing.Font("Action Jackson", 47.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Font = new System.Drawing.Font("Action Jackson", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Title.ForeColor = System.Drawing.Color.White;
-            this.Title.Location = new System.Drawing.Point(203, 36);
+            this.Title.Location = new System.Drawing.Point(95, 53);
             this.Title.Name = "Title";
-            this.Title.Size = new System.Drawing.Size(193, 65);
+            this.Title.Size = new System.Drawing.Size(413, 41);
             this.Title.TabIndex = 1;
-            this.Title.Text = "Help";
+            this.Title.Text = "Get help with...";
             // 
             // backLabel
             // 
@@ -64,12 +68,64 @@
             this.helpText.BackColor = System.Drawing.Color.Transparent;
             this.helpText.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.helpText.ForeColor = System.Drawing.Color.White;
-            this.helpText.Location = new System.Drawing.Point(54, 118);
+            this.helpText.Location = new System.Drawing.Point(54, 206);
             this.helpText.Name = "helpText";
-            this.helpText.Size = new System.Drawing.Size(488, 217);
+            this.helpText.Size = new System.Drawing.Size(488, 149);
             this.helpText.TabIndex = 5;
             this.helpText.Text = "This is a user\'s manual! It explains the functionality of each of the menus and h" +
     "ow to play the game. This text will be altered before the beta release!";
+            this.helpText.Visible = false;
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.headerLabel.Font = new System.Drawing.Font("Action Jackson", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.ForeColor = System.Drawing.Color.White;
+            this.headerLabel.Location = new System.Drawing.Point(51, 134);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(238, 32);
+            this.headerLabel.TabIndex = 6;
+            this.headerLabel.Text = "Menu title";
+            // 
+            // statisticsLabel
+            // 
+            this.statisticsLabel.AutoSize = true;
+            this.statisticsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.statisticsLabel.Font = new System.Drawing.Font("Action Jackson", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statisticsLabel.ForeColor = System.Drawing.Color.White;
+            this.statisticsLabel.Location = new System.Drawing.Point(188, 209);
+            this.statisticsLabel.Name = "statisticsLabel";
+            this.statisticsLabel.Size = new System.Drawing.Size(213, 32);
+            this.statisticsLabel.TabIndex = 7;
+            this.statisticsLabel.Text = "Statistics";
+            this.statisticsLabel.Click += new System.EventHandler(this.statisticsLabel_Click);
+            // 
+            // settingsLabel
+            // 
+            this.settingsLabel.AutoSize = true;
+            this.settingsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.settingsLabel.Font = new System.Drawing.Font("Action Jackson", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.settingsLabel.ForeColor = System.Drawing.Color.White;
+            this.settingsLabel.Location = new System.Drawing.Point(202, 274);
+            this.settingsLabel.Name = "settingsLabel";
+            this.settingsLabel.Size = new System.Drawing.Size(177, 32);
+            this.settingsLabel.TabIndex = 8;
+            this.settingsLabel.Text = "Settings";
+            this.settingsLabel.Click += new System.EventHandler(this.settingsLabel_Click);
+            // 
+            // howToLabel
+            // 
+            this.howToLabel.AutoSize = true;
+            this.howToLabel.BackColor = System.Drawing.Color.Transparent;
+            this.howToLabel.Font = new System.Drawing.Font("Action Jackson", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.howToLabel.ForeColor = System.Drawing.Color.White;
+            this.howToLabel.Location = new System.Drawing.Point(172, 151);
+            this.howToLabel.Name = "howToLabel";
+            this.howToLabel.Size = new System.Drawing.Size(259, 32);
+            this.howToLabel.TabIndex = 9;
+            this.howToLabel.Text = "How to play";
+            this.howToLabel.Click += new System.EventHandler(this.howToLabel_Click);
             // 
             // Help_t
             // 
@@ -77,6 +133,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(599, 399);
+            this.Controls.Add(this.howToLabel);
+            this.Controls.Add(this.settingsLabel);
+            this.Controls.Add(this.statisticsLabel);
+            this.Controls.Add(this.headerLabel);
             this.Controls.Add(this.helpText);
             this.Controls.Add(this.backLabel);
             this.Controls.Add(this.Title);
@@ -93,5 +153,9 @@
         private System.Windows.Forms.Label Title;
         private System.Windows.Forms.Label backLabel;
         private System.Windows.Forms.Label helpText;
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.Label statisticsLabel;
+        private System.Windows.Forms.Label settingsLabel;
+        private System.Windows.Forms.Label howToLabel;
     }
 }
--- StretchIt/Help.cs
+++ StretchIt/Help.cs
@@ -11,23 +11,74 @@ namespace StretchIt
 {
     public partial class Help_t : Form
     {
+        private string statistics_help_text = "The statistics screen allows " +
+            "you to track your all time score and see the difference " +
+        "between all of your scores and your current score.";
+        private string settings_help_text = "Settings help text";
+        private string howTo_help_text = "This is how to play";
+        private bool is_nested = false;
+
         public Help_t()
         {
             InitializeComponent();
             this.Visible = false;
+            // help sub-menu fields should not be visible
+            this.headerLabel.Visible = false;
+            this.helpText.Visible = false;
         }
 
         private void backLabel_Click(object sender, EventArgs e)
         {
-            this.Visible = false;
-            GlobalVar.MAIN_MENU.Visible = true;
-            GlobalVar.MAIN_MENU.Activate();
+            if (is_nested == false)
+            {
+                this.Visible = false;
+                GlobalVar.MAIN_MENU.Visible = true;
+                GlobalVar.MAIN_MENU.Activate();
+            } else
+            {
+                toggle_help_menu();
+            }
+        }
+
+        private void toggle_help_menu()
+        {
+            is_nested = !is_nested;
+            // help sub-menu fields
+            this.headerLabel.Visible = !this.headerLabel.Visible;
+            this.helpText.Visible = !this.helpText.Visible;
+            // help header labels
+            this.statisticsLabel.Visible = !this.statisticsLabel.Visible;
+            this.settingsLabel.Visible = !this.settingsLabel.Visible;
+            this.howToLabel.Visible = !this.howToLabel.Visible;
+
         }
 
         private void Help_t_FormClosing(object sender, FormClosingEventArgs e)
         {
             e.Cancel = true;
             MessageBox.Show("You must exit via the Back button.");
+        }
+
+        private void statisticsLabel_Click(object sender, EventArgs e)
+        {
+            headerLabel.Text = "Statistics";
+            helpText.Text = statistics_help_text;
+            toggle_help_menu();
+        }
+
+        private void settingsLabel_Click(object sender, EventArgs e)
+        {
+            headerLabel.Text = "Settings";
+            helpText.Text = settings_help_text;
+            toggle_help_menu();
+        }
+
+        private void howToLabel_Click(object sender, EventArgs e)
+        {
+            headerLabel.Text = "How to play";
+            helpText.Text = howTo_help_text;
+            toggle_help_menu();
+
         }
 
 
