09aaa179afe2777ee7c3bb6b84665b3bc16d9794
Michael Meyerson <mikemey@umich.edu>
2014-11-24 22:27:51 -0500

    Pick from multiple Audio celebrations

--- StretchIt/GlobalVar.cs
+++ StretchIt/GlobalVar.cs
@@ -31,8 +31,8 @@ namespace StretchIt
         public const string             SETTINGS_PATH_C = @"..\..\settings.txt";
         public const string             TEMP_GESTURE_FILE = @"..\..\GestureImages\gesture.txt";
         public const string             TEMP_GESTURE_IMAGE_FILE = @"..\..\GestureImages\gesture.png";
-        public const string             CELEB_AUDIO_FILE = @"..\..\celebration.wav";
-        public const string             CELEB_IMAGE_FILE = @"..\..\celebration.jpg";
+        public const string[]           CELEB_AUDIO_FILES = Directory.GetFiles(@"..\..\Celebrations", "celebration*.wav");
+        public const string             CELEB_IMAGE_FILE = @"..\..\Celebrations\celebration1.jpg";
         public const int                NUM_PIXELS_C = 307200;
         public const int                NUM_FRAMES_RECORD_C = 100;
         public const int                NUM_GESTURES_IN_GAME_C = 15;
